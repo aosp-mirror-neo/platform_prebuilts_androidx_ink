e4426eaaf5dc37b849ad1eff919537f7c46ae3b2
Ink Releaser <mdb.ink-eng-team-releaser@google.com>
2026-08-11 10:15:49 -0700

    Automatic import from libink.so_20260811_0128_RC00
    https://rapid.corp.google.com/#candidate/libink.so/libink.so_20260811_0128/libink.so_20260811_0128_RC00
    
    Updates Ink native prebuilts
    This build IS suitable for public release.
    
    Change-Id: I49715be086b9fbefe4bccbe0f0aa9caac1d5a1ce

--- native/headers/ink_cinterop.h
+++ native/headers/ink_cinterop.h
@@ -842,9 +842,9 @@ typedef struct {
 } AffineTransformNative_Parallelogram;
 
 AffineTransformNative_Parallelogram AffineTransformNative_apply(
-    float a, float b, float c, float d, float e, float f, float quad_center_x,
-    float quad_center_y, float quad_width, float quad_height,
-    float quad_rotation_degrees, float quad_shear_factor);
+    float m00, float m10, float m20, float m01, float m11, float m21,
+    float quad_center_x, float quad_center_y, float quad_width,
+    float quad_height, float quad_rotation_degrees, float quad_shear_factor);
 
 #ifdef __cplusplus
 }  // extern "C"
@@ -1575,6 +1575,85 @@ void StatusNative_throwExceptionFromUnknownStatusCodeForTesting(
 #endif
 
 #endif  // THIRD_PARTY_INK_KMP_STATUS_NATIVE_H_
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef THIRD_PARTY_INK_RENDERING_METAL_METAL_RENDERER_NATIVE_H_
+#define THIRD_PARTY_INK_RENDERING_METAL_METAL_RENDERER_NATIVE_H_
+
+// C-compatible library header for Kotlin-native bindings.
+
+#include <simd/types.h>
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Creates a heap-allocated `ink::rendering::MetalRenderer`, returning a raw
+// pointer to it. `device` is a pointer to a MTLDevice to use for rendering.
+// `color_pixel_format` and `stencil_pixel_format` are the `MTLPixelFormat` of
+// the color and stencil textures to render to. `sample_count` is the number of
+// samples per pixel for MSAA. If -1, shader-based antialiasing will be used
+// instead.
+int64_t MetalRendererNative_create(
+    void* device, uint64_t color_pixel_format, uint64_t stencil_pixel_format,
+    int sample_count,
+    void (*throw_from_status_callback)(void* jni_env, int status_code,
+                                       const char* status_str));
+
+// Draws an in-progress stroke using the given render encoder. `render_encoder`
+// is a pointer to a MTLRenderCommandEncoder. `in_progress_stroke_native_ptr` is
+// a raw pointer to a native `ink::InProgressStroke`. The remaining parameters
+// are the elements of the model, view, and projection transforms.
+void MetalRendererNative_drawInProgressStroke(
+    int64_t native_ptr, void* render_encoder,
+    int64_t in_progress_stroke_native_ptr, float model_transform_m00,
+    float model_transform_m10, float model_transform_m20,
+    float model_transform_m01, float model_transform_m11,
+    float model_transform_m21, float view_transform_m00,
+    float view_transform_m10, float view_transform_m20,
+    float view_transform_m01, float view_transform_m11,
+    float view_transform_m21, float projection_transform_m00,
+    float projection_transform_m10, float projection_transform_m20,
+    float projection_transform_m01, float projection_transform_m11,
+    float projection_transform_m21);
+
+// Draws a completed stroke using the given render encoder. `render_encoder`
+// is a pointer to a MTLRenderCommandEncoder. `stroke_native_ptr` is a raw
+// pointer to a native `ink::Stroke`. The remaining parameters are the elements
+// of the model, view, and projection transforms.
+void MetalRendererNative_drawStroke(
+    int64_t native_ptr, void* render_encoder, int64_t stroke_native_ptr,
+    float model_transform_m00, float model_transform_m10,
+    float model_transform_m20, float model_transform_m01,
+    float model_transform_m11, float model_transform_m21,
+    float view_transform_m00, float view_transform_m10,
+    float view_transform_m20, float view_transform_m01,
+    float view_transform_m11, float view_transform_m21,
+    float projection_transform_m00, float projection_transform_m10,
+    float projection_transform_m20, float projection_transform_m01,
+    float projection_transform_m11, float projection_transform_m21);
+
+// Deletes the heap-allocated `ink::rendering::MetalRenderer`.
+void MetalRendererNative_free(int64_t native_ptr);
+
+#ifdef __cplusplus
+}  // extern "C"
+#endif
+
+#endif  // THIRD_PARTY_INK_RENDERING_METAL_METAL_RENDERER_NATIVE_H_
 // Copyright 2026 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
